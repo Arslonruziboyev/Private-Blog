dd9b641f59085cdee3e1ee197eb17d6f3842f0e3
Arslonruziboyev <arslonruziboyev@gmail.com>
2021-12-11 22:30:59 +0500

    changed

---
+++ js/app.js
@@ -0,0 +1,61 @@
+
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  
+  links.classList.toggle("show-links");
+});
+
+var typed = new Typed('#typed', {
+  strings: ['Hello, How are you.', 'Welcome to my blog.'],
+  typeSpeed: 80,
+  startDelay: 3000,
+  backSpeed: 80,
+  smartBackspace: true,
+  shuffle: false,
+  backDelay: 700,
+  fadeOut: false,
+  fadeOutClass: 'typed-fade-out',
+  fadeOutDelay: 500,
+  loop: true,
+  loopCount: Infinity,
+  showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true,
+});
+
+//year
+
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  // setup back to top link
+
+  if (scrollHeight > 500) {
+    // console.log("helo");
+
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
+});
